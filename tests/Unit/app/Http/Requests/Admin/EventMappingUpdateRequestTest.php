74712322ff71651dd4a35e26ba72cdf1f045fa68
David Eberhardt <david.eberhardt@outlook.com>
2025-08-20 18:25:08 +0100

    FIX: Enhance EventMappingUpdateRequestTest with additional tests for external_id rule closures
    Signed-off-by: David Eberhardt <david.eberhardt@outlook.com>

--- tests/Unit/app/Http/Requests/Admin/EventMappingUpdateRequestTest.php
+++ tests/Unit/app/Http/Requests/Admin/EventMappingUpdateRequestTest.php
@@ -4,32 +4,44 @@ namespace Tests\Unit\app\Http\Requests\Admin;
 
 use Tests\TestCase;
 use App\Http\Requests\Admin\EventMappingUpdateRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\TicketProvider;
 
 class EventMappingUpdateRequestTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testAuthorizeReturnsTrue()
     {
-        $request = new EventMappingUpdateRequest();
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
         $this->assertTrue($request->authorize());
     }
 
     public function testRulesContainsExternalId()
     {
-        $request = new EventMappingUpdateRequest();
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
         $rules = $request->rules();
         $this->assertArrayHasKey('external_id', $rules);
     }
 
     public function testExternalIdRuleIncludesRequired()
     {
-        $request = new EventMappingUpdateRequest();
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
         $rule = $request->rules()['external_id'];
         $this->assertContains('required', $rule);
     }
 
     public function testExternalIdRuleClosureFailsIfFormatInvalid()
     {
-        $request = new EventMappingUpdateRequest();
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
         $rules = $request->rules();
         $closure = null;
         foreach ($rules['external_id'] as $rule) {
@@ -52,110 +64,119 @@ class EventMappingUpdateRequestTest extends TestCase
         $this->assertTrue($called, 'Fail closure was not called for invalid format');
     }
 
-    // FIXME SQL Error? (Connection: sqlite, SQL: select * from "ticket_providers" where "id" = 1 limit 1)
+    public function testExternalIdRuleClosureFailsIfProviderDoesNotExist()
+    {
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
+        // Simulate event property with an object that has the method
+        $request->event = new class {
+            public function getAvailableEventMappings($mapping = null)
+            {
+                return [];
+            }
+        };
 
-    // public function testExternalIdRuleClosureFailsIfProviderDoesNotExist()
-    // {
-    //     $request = $this->getMockBuilder(EventMappingUpdateRequest::class)
-    //         ->onlyMethods(['__get'])
-    //         ->getMock();
-    //     // Simulate event property
-    //     $request->event = (object)[
-    //         'getAvailableEventMappings' => function () {
-    //             return [];
-    //         }
-    //     ];
-    //     \Mockery::mock(['alias' => 'App\\Models\\TicketProvider'])
-    //         ->shouldReceive('whereId')->with(999)->andReturnSelf()
-    //         ->shouldReceive('first')->andReturn(null);
+        $rules = $request->rules();
+        $closure = null;
+        foreach ($rules['external_id'] as $rule) {
+            if ($rule instanceof \Closure) {
+                $closure = $rule;
+                break;
+            }
+        }
+        $called = false;
+        $fail = function ($message) use (&$called) {
+            $called = true;
+            \PHPUnit\Framework\Assert::assertEquals('Ticket Provider does not exist', $message);
+        };
+        $closure('external_id', '999:abc', $fail);
+        $this->assertTrue($called, 'Fail closure was not called for missing provider');
+    }
 
-    //     $rules = $request->rules();
-    //     $closure = null;
-    //     foreach ($rules['external_id'] as $rule) {
-    //         if ($rule instanceof \Closure) {
-    //             $closure = $rule;
-    //             break;
-    //         }
-    //     }
-    //     $called = false;
-    //     $fail = function ($message) use (&$called) {
-    //         $called = true;
-    //         \PHPUnit\Framework\Assert::assertEquals('Ticket Provider does not exist', $message);
-    //     };
-    //     $closure('external_id', '999:abc', $fail);
-    //     $this->assertTrue($called, 'Fail closure was not called for missing provider');
-    // }
+    public function testExternalIdRuleClosureFailsIfEventAlreadyMapped()
+    {
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
 
-    // public function testExternalIdRuleClosureFailsIfEventAlreadyMapped()
-    // {
-    //     $request = $this->getMockBuilder(EventMappingUpdateRequest::class)
-    //         ->onlyMethods(['__get'])
-    //         ->getMock();
-    //     $mockProvider = (object)['id' => 1];
-    //     $mockEvent = (object)['id' => 'abc'];
-    //     $mockMapping = (object)[
-    //         'provider' => $mockProvider,
-    //         'events' => [$mockEvent]
-    //     ];
-    //     $request->event = (object)[
-    //         'getAvailableEventMappings' => function () use ($mockMapping) {
-    //             return [$mockMapping];
-    //         }
-    //     ];
-    //     \Mockery::mock(['alias' => 'App\\Models\\TicketProvider'])
-    //         ->shouldReceive('whereId')->with(1)->andReturnSelf()
-    //         ->shouldReceive('first')->andReturn($mockProvider);
+        // Create a real TicketProvider using factory
+        $provider = TicketProvider::factory()->create();
+        $mockEvent = (object)['id' => 'abc'];
+        $mockMapping = (object)[
+            'provider' => $provider,
+            'events' => [$mockEvent]
+        ];
 
-    //     $rules = $request->rules();
-    //     $closure = null;
-    //     foreach ($rules['external_id'] as $rule) {
-    //         if ($rule instanceof \Closure) {
-    //             $closure = $rule;
-    //             break;
-    //         }
-    //     }
-    //     $called = false;
-    //     $fail = function ($message) use (&$called) {
-    //         $called = true;
-    //         \PHPUnit\Framework\Assert::assertEquals('That provider event is already mapped', $message);
-    //     };
-    //     $closure('external_id', '1:xyz', $fail);
-    //     $this->assertTrue($called, 'Fail closure was not called for already mapped event');
-    // }
+        // Event object that returns available mappings
+        $request->event = new class($mockMapping) {
+            private $mapping;
+            public function __construct($m)
+            {
+                $this->mapping = $m;
+            }
+            public function getAvailableEventMappings($mapping = null)
+            {
+                return [$this->mapping];
+            }
+        };
 
-    // public function testExternalIdRuleClosurePassesForValidMapping()
-    // {
-    //     $request = $this->getMockBuilder(EventMappingUpdateRequest::class)
-    //         ->onlyMethods(['__get'])
-    //         ->getMock();
-    //     $mockProvider = (object)['id' => 1];
-    //     $mockEvent = (object)['id' => 'abc'];
-    //     $mockMapping = (object)[
-    //         'provider' => $mockProvider,
-    //         'events' => [$mockEvent]
-    //     ];
-    //     $request->event = (object)[
-    //         'getAvailableEventMappings' => function () use ($mockMapping) {
-    //             return [$mockMapping];
-    //         }
-    //     ];
-    //     \Mockery::mock(['alias' => 'App\\Models\\TicketProvider'])
-    //         ->shouldReceive('whereId')->with(1)->andReturnSelf()
-    //         ->shouldReceive('first')->andReturn($mockProvider);
+        $rules = $request->rules();
+        $closure = null;
+        foreach ($rules['external_id'] as $rule) {
+            if ($rule instanceof \Closure) {
+                $closure = $rule;
+                break;
+            }
+        }
+        $called = false;
+        $fail = function ($message) use (&$called) {
+            $called = true;
+            \PHPUnit\Framework\Assert::assertEquals('That provider event is already mapped', $message);
+        };
+        $closure('external_id', $provider->id . ':xyz', $fail);
+        $this->assertTrue($called, 'Fail closure was not called for already mapped event');
+    }
 
-    //     $rules = $request->rules();
-    //     $closure = null;
-    //     foreach ($rules['external_id'] as $rule) {
-    //         if ($rule instanceof \Closure) {
-    //             $closure = $rule;
-    //             break;
-    //         }
-    //     }
-    //     $fail = function ($message) {
-    //         \PHPUnit\Framework\Assert::fail('Fail closure should not be called for valid mapping');
-    //     };
-    //     // Should not call fail for a valid mapping
-    //     $closure('external_id', '1:abc', $fail);
-    //     $this->assertTrue(true); // If no exception, test passes
-    // }
+    public function testExternalIdRuleClosurePassesForValidMapping()
+    {
+        $request = new class extends EventMappingUpdateRequest {
+            public $event;
+        };
+
+        // Create a real TicketProvider using factory
+        $provider = TicketProvider::factory()->create();
+        $mockEvent = (object)['id' => 'abc'];
+        $mockMapping = (object)[
+            'provider' => $provider,
+            'events' => [$mockEvent]
+        ];
+
+        $request->event = new class($mockMapping) {
+            private $mapping;
+            public function __construct($m)
+            {
+                $this->mapping = $m;
+            }
+            public function getAvailableEventMappings($mapping = null)
+            {
+                return [$this->mapping];
+            }
+        };
+
+        $rules = $request->rules();
+        $closure = null;
+        foreach ($rules['external_id'] as $rule) {
+            if ($rule instanceof \Closure) {
+                $closure = $rule;
+                break;
+            }
+        }
+        $fail = function ($message) {
+            \PHPUnit\Framework\Assert::fail('Fail closure should not be called for valid mapping');
+        };
+        // Should not call fail for a valid mapping
+        $closure('external_id', $provider->id . ':abc', $fail);
+        $this->assertTrue(true); // If no exception, test passes
+    }
 }
