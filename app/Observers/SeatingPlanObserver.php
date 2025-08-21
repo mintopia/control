d92ca072a3a60ff8c40b53117aa0db973ebe26e0
David Eberhardt <david.eberhardt@outlook.com>
2025-08-21 11:19:28 +0100

    FIX: ClanObserverTest - Correct logic for incrementing revision in SeatingPlanObserver to ensure it only increments when the model exists and the revision is dirty
    Signed-off-by: David Eberhardt <david.eberhardt@outlook.com>

--- app/Observers/SeatingPlanObserver.php
+++ app/Observers/SeatingPlanObserver.php
@@ -14,9 +14,10 @@ class SeatingPlanObserver
             $seatingPlan->code = makePermalink($seatingPlan->name);
         }
         // Increment the revision when the revision attribute was changed
-        // (i.e. isDirty). Previously this was inverted and incremented when
-        // it was not dirty.
-        if ($seatingPlan->isDirty('revision')) {
+        // (i.e. isDirty) and the model already exists (not on initial create).
+        // This avoids bumping the revision during factory/model creation where
+        // the attribute will be considered dirty.
+        if ($seatingPlan->exists && $seatingPlan->isDirty('revision')) {
             $seatingPlan->revision++;
         }
     }
